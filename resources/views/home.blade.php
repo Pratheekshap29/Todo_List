057a30f007308476d70692864866f126106a0b91
Pratheekshap29 <68225339+Pratheekshap29@users.noreply.github.com>
2020-12-04 00:15:57 +0530

    notification

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,28 @@
         </div>
     </div>
 </div>
-
-
+<div class="container pt-4">
+@php
+use Carbon\Carbon;
+$current=new Carbon();
+$current->timezone('Asia/Kolkata');
+@endphp
+@foreach ($todos as $todo)
+    @if($current >= $todo->startTime)
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
+    <div class="toast-header">
+        <img src="..." class="rounded mr-2" alt="...">
+        <strong class="mr-auto">{{$todo->title}}</strong>
+        <small class="text-muted">{{$todo->startTime}}</small>
+        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="toast-body">
+        See? Just like this.
+    </div>
+    </div>
+    @endif
+@endforeach
 </div>
-</div>
+@endsection
